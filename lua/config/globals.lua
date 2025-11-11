ad9426938101eef379f47d781f05a915dfd3fa90
Alexis12119 <corporal461@gmail.com>
2025-11-11 08:41:41 +0800

    feat: don't use iteration for the options and globals.

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -1,23 +1,17 @@
 -- NOTE: Global Variables
+
 local sysname = vim.uv.os_uname().sysname
-local os = sysname:match("Windows") and "Windows" or sysname:match("Linux") and "Linux" or sysname -- Windows, Linux, Darwin, NetBSD,...
+local os = sysname:match("Windows") and "Windows" or sysname:match("Linux") and "Linux" or sysname -- Windows, Linux, Darwin, NetBSD, etc.
 local is_windows = os == "Windows"
 
-local global = {
-  snipmate_snippets_path = vim.fn.stdpath("config") .. "/snippets/snipmate", -- path to snipmate snippets
-  skip_ts_context_commentstring_module = true, -- disable treesitter context commentstring
-  ["loaded_node_provider"] = 0, -- disable node provider
-  ["loaded_python3_provider"] = 0, -- disable python3 provider
-  ["loaded_perl_provider"] = 0, -- disable perl provider
-  ["loaded_ruby_provider"] = 0, -- disable ruby provider
-  toggle_theme_icon = "   ",
-  os = os,
-  is_windows = is_windows,
-  path_delimiter = is_windows and ";" or ":",
-  path_separator = is_windows and "\\" or "/",
-  ai_cmp = false,
-}
-
-for name, value in pairs(global) do
-  vim.g[name] = value
-end
+vim.g.skip_ts_context_commentstring_module = true -- disable treesitter context commentstring
+vim.g.loaded_node_provider = 0 -- disable node provider
+vim.g.loaded_python3_provider = 0 -- disable python3 provider
+vim.g.loaded_perl_provider = 0 -- disable perl provider
+vim.g.loaded_ruby_provider = 0 -- disable ruby provider
+vim.g.toggle_theme_icon = "   "
+vim.g.os = os
+vim.g.is_windows = is_windows
+vim.g.path_delimiter = is_windows and ";" or ":"
+vim.g.path_separator = is_windows and "\\" or "/"
+vim.g.ai_cmp = false
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,60 +2,47 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- NOTE: Neovim options
+local opt = vim.opt
 
-local options = {
-  laststatus = 3, -- global statusline
-  backup = false, -- creates a backup file
-  conceallevel = 0, -- so that `` is visible in markdown files
-  fileencoding = "utf-8", -- the encoding written to a file
-  hidden = true, -- required to keep multiple buffers and open multiple buffers
-  ignorecase = true, -- ignore case in search patterns
-  mouse = "a", -- allow the mouse to be used in neovim
-  pumheight = 8, -- pop up menu height
-  pumblend = 0, -- transparency of pop-up menu
-  showmode = false, -- we don't need to see things like -- INSERT -- anymore
-  smartcase = true, -- smart case
-  smartindent = true, -- make indenting smarter again
-  splitbelow = true, -- force all horizontal splits to go below current window
-  splitright = true, -- force all vertical splits to go to the right of current window
-  splitkeep = "screen", -- don't automatically close empty splits
-  swapfile = true, -- creates a swapfile
-  timeoutlen = 500, -- time to wait for a mapped sequence to complete (in milliseconds)
-  undofile = true, -- enable persistent undo
-  updatetime = 100, -- faster completion (4000ms default)
-  writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true, -- convert tabs to spaces
-  shiftwidth = 2, -- the number of spaces inserted for each indentation
-  tabstop = 2, -- insert 2 spaces for a tab
-  cursorline = true, -- highlight the current line
-  number = true, -- set numbered lines
-  relativenumber = true, -- set relative numbered lines
-  numberwidth = 5, -- set number column width to 4 {default 4}
-  signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-  wrap = false, -- display lines as one long line
-  -- scrolloff = 4, -- minimal number of columns to scroll horizontally.
-  -- sidescrolloff = 4, -- minimal number of screen columns
-  lazyredraw = false, -- Won't be redrawn while executing macros, register and other commands.
-  termguicolors = true, -- Enables 24-bit RGB color in the TUI
-  fillchars = { eob = " ", fold = " ", foldopen = "", foldsep = " ", foldclose = "", lastline = " " }, -- make EndOfBuffer invisible
-  ruler = false, -- show the line and column number of the cursor position``
-  cmdheight = 0, -- height of the command line
-  helpheight = 10, -- height of the help window
-  sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions",
-  -- shell = vim.fn.executable "pwsh" and "pwsh" or "powershell",
-  -- shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-  -- shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-  -- shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-  -- shellquote = "",
-  -- shellxquote = "",
-}
+opt.laststatus = 3
+opt.backup = false
+opt.conceallevel = 0
+opt.fileencoding = "utf-8"
+opt.hidden = true
+opt.ignorecase = true
+opt.mouse = "a"
+opt.pumheight = 8
+opt.pumblend = 0
+opt.showmode = false
+opt.smartcase = true
+opt.smartindent = true
+opt.splitbelow = true
+opt.splitright = true
+opt.splitkeep = "screen"
+opt.swapfile = true
+opt.timeoutlen = 500
+opt.undofile = true
+opt.updatetime = 100
+opt.writebackup = false
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.cursorline = true
+opt.number = true
+opt.relativenumber = true
+opt.numberwidth = 5
+opt.signcolumn = "yes"
+opt.wrap = false
+opt.lazyredraw = false
+opt.termguicolors = true
+opt.fillchars = { eob = " ", fold = " ", foldopen = "", foldsep = " ", foldclose = "", lastline = " " }
+opt.ruler = false
+opt.cmdheight = 0
+opt.helpheight = 10
+opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
-vim.opt.shortmess:append("Ac") -- Disable asking
-vim.opt.whichwrap:append("<>[]hl")
-vim.opt.iskeyword:append("-")
-
-for name, value in pairs(options) do
-  vim.opt[name] = value
-end
+opt.shortmess:append("Ac")
+opt.whichwrap:append("<>[]hl")
+opt.iskeyword:append("-")
 
 require("config.globals")
